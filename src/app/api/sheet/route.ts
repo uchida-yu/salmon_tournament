5cb1c8e6403fca14aaf2684a793edbfd669d0079
uchida-yu <yu3shabadaba+github@gmail.com>
2024-11-24 05:33:23 +0900

    log削除

--- src/app/api/sheet/route.ts
+++ src/app/api/sheet/route.ts
@@ -8,37 +8,26 @@ export async function GET() {
   const private_key = process.env.PRIVATE_KEY;
   const auth = new google.auth.JWT(client_email, '', private_key, ['https://www.googleapis.com/auth/spreadsheets.readonly']);
   const spreadsheetId = process.env.SPREADSHEET_ID;
-  let token: any = undefined; // FIXME: 消す
 
   try {
-    // // 認証トークンの取得
-    // token = await auth.authorize();
+    // 認証トークンの取得
+    const token = await auth.authorize();
 
-    // // リクエストURL
-    // const range = 'Sheet1!A:J'; // 取得する範囲
-    // const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}`;
+    // リクエストURL
+    const range = 'Sheet1!A:J'; // 取得する範囲
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}`;
 
-    // // HTTPリクエスト
-    // const response = await fetch(url, {
-    //   headers: {
-    //     Authorization: `Bearer ${token.access_token}`,
-    //   },
-    // }).then((res) => res.json());
+    // HTTPリクエスト
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token.access_token}`,
+      },
+    }).then((res) => res.json());
 
     return NextResponse.json({
-      response: {
-        client_email, // FIXME: 消す↓
-        private_key,
-        token
-      },
+      response,
     });
   } catch (error) {
     console.error('APIエラー:', error);
-    return NextResponse.json({
-      error,
-      client_email, // FIXME: 消す↓
-      private_key,
-      token,
-     }, { status: 500 });
   }
 }
