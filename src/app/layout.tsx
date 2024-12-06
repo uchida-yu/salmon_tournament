eefaf00d3c6ad376f9944350de7abeee334e6751
uchida-yu <yu3shabadaba+github@gmail.com>
2024-12-06 19:01:27 +0900

    analytics fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Script from "next/script";
-
-const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+import Analytics from "./ui/component/Analytics";
 
 export const metadata: Metadata = {
   title: "サーモンランタイカイ検索",
@@ -17,18 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
-          `}
-        </Script>
+        <Analytics />
       </head>
       <body>{children}</body>
     </html>
