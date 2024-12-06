9a97021c628bf7808ba74ac6cd0788588ee2b191
uchida-yu <yu3shabadaba+github@gmail.com>
2024-12-06 19:59:18 +0900

    analytics fixed

--- src/app/ui/component/Analytics.tsx
+++ src/app/ui/component/Analytics.tsx
@@ -1,32 +1,23 @@
 "use client";
-
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
 import Script from "next/script";
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
-declare global {
-  interface Window {
-    gtag: any;
-  }
-}
-
 export default function Analytics() {
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if (typeof window.gtag === "function") {
-      window.gtag("config", GA_TRACKING_ID, {
-        page_path: pathname,
-      });
-    }
-  }, [pathname]);
-
   return (
-    <Script
-      src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
+    </>
   );
 }
