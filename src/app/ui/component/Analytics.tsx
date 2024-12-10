b25ce823a9172123fcf59ab7b5d176d72be9b43f
uchida-yu <yu3shabadaba+github@gmail.com>
2024-12-10 15:03:59 +0900

    tag test

--- src/app/ui/component/Analytics.tsx
+++ src/app/ui/component/Analytics.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 
 export default function Analytics() {
   const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+  console.log("GA_TRACKING_ID", GA_TRACKING_ID);
   return (
     <>
       <Script
@@ -11,7 +12,6 @@ export default function Analytics() {
       />
       <Script>
         {`
-          console.log('gtag', '${GA_TRACKING_ID}');
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
