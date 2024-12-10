58fd2557367810302035f15c6f5017f4088713ea
uchida-yu <yu3shabadaba+github@gmail.com>
2024-12-10 14:44:16 +0900

    test

--- src/app/ui/component/Analytics.tsx
+++ src/app/ui/component/Analytics.tsx
@@ -12,6 +12,7 @@ export default function Analytics() {
       />
       <Script>
         {`
+          console.log('gtag', GA_TRACKING_ID);
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
